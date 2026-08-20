e28d8125efd18dd700a63fe96dd18b75afeff205
RMcVelia <rick.mcvelia@gmail.com>
2026-08-20 12:57:32 +0100

    Just testing premium front door

--- domains/infrastructure/front_door.tf
+++ domains/infrastructure/front_door.tf
@@ -2,7 +2,7 @@ resource "azurerm_cdn_frontdoor_profile" "main" {
   for_each            = var.hosted_zone
   name                = each.value.front_door_name
   resource_group_name = each.value.resource_group_name
-  sku_name            = "Standard_AzureFrontDoor"
+  sku_name            = "Premium_AzureFrontDoor"
   tags                = var.tags
 
   lifecycle { ignore_changes = [tags] }
